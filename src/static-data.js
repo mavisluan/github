d039a2feafea90d92dcb81103e6012c877e0cd73
Yi Angelov <mavisluan83@gmail.com>
2018-08-15 20:58:48 -0700

    add static-data

---
+++ src/static-data.js
@@ -0,0 +1,9 @@
+export const items = [
+    { id: 0, type: 'folder', name: 'public', text: 'add fontawesome to index.html', timestamp: '2018-08-15 16:08'},
+    { id: 1, type: 'folder', name: 'src', text: 'add files via upload', timestamp: '2018-08-15 19:52'},
+    { id: 2, type: 'file', name: '.gitignore', text: 'starter of the project', timestamp: '2018-08-15 16:08'},
+    { id: 3, type: 'file', name: 'README.md', text: 'update README.md', timestamp: '2018-08-15 20:19'},
+    { id: 4, type: 'file', name: 'package.json', text: 'add moment', timestamp: '2018-08-15 19:17'},
+    { id: 5, type: 'file', name: 'yarn.lock', text: 'update Card with Time Component', timestamp: '2018-08-15 19:17'},
+]
+
