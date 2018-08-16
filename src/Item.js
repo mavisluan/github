693a9e0f58a833ccbc362fc39d643761cbfbd077
Yi Angelov <mavisluan83@gmail.com>
2018-08-15 21:45:38 -0700

    update Item

--- src/Item.js
+++ src/Item.js
@@ -1,11 +1,11 @@
 import React from 'react'
+import ItemName from './ItemName'
 
 const Item = ({ item: {type, name, text, timestamp }}) => {
     return (
         <div className='item'>
             <div className='item-name'>
-                <span>{type}</span>
-                <span>{name}</span>
+                <ItemName type={type} name={name} />
             </div>
             <div className='text'>
                 <span>{text}</span>
