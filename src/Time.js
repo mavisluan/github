01c4a47b78b0ccbb7aa335b0910aa94005d688f9
Yi Angelov <mavisluan83@gmail.com>
2018-08-15 22:03:47 -0700

    add Time component

---
+++ src/Time.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import moment from 'moment'
+
+const Time = ({ time }) => {
+    const timeString = moment(time).fromNow()
+    return (
+        <span>{timeString}</span>
+    )
+}
+
+export default Time
